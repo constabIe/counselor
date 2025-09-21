d8bcbaf55cbc0d6674fcc696e9e1689e66eb57d5
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-09-21 12:52:11 +0300

    1

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,9 +1,5 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-# Загружаем переменные окружения из .env файла
-load_dotenv()
 
 from src.config_schema import Settings
 
@@ -11,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 settings_path = os.getenv("SETTINGS_PATH", BASE_DIR / ".env")
 
-settings_path = os.getenv("SETTINGS_PATH", ".env")
-settings: Settings = Settings.from_env(Path(settings_path)) if Path(settings_path).exists() else Settings() # type: ignore
-
+settings: Settings = (
+    Settings.from_env(settings_path) if Path(settings_path).exists() else Settings()  # type: ignore
+)
 
 def get_settings() -> Settings:
     """Получить настройки приложения"""
