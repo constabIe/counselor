ce8a26fdd8ad0cfedf930131987c95f091876a8e
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-09-21 12:41:06 +0300

    1

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,13 +3,10 @@ from pathlib import Path
 
 from src.config_schema import Settings
 
-# settings_path = os.getenv("SETTINGS_PATH", "settings.yaml")
-# settings: Settings = Settings.from_yaml(Path(settings_path)) if Path(settings_path).exists() else Settings() # type: ignore
+BASE_DIR = Path(__file__).resolve().parent
 
+settings_path = os.getenv("SETTINGS_PATH", BASE_DIR / ".env")
 
-settings_path = os.getenv("SETTINGS_PATH", ".env")
-print(settings_path, Path(settings_path).exists())
-settings: Settings = Settings.from_env(Path(settings_path)) if Path(settings_path).exists() else Settings() # type: ignore
-print(settings.db_url.get_secret_value())
-print(settings.jwt_secret_key.get_secret_value())
-
+settings: Settings = (
+    Settings.from_env(settings_path) if Path(settings_path).exists() else Settings()  # type: ignore
+)
