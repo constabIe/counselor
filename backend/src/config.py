73e69346113839cb6dd49bb7d92ea6f0d1b8511b
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-09-21 12:45:45 +0300

    1

--- backend/src/config.py
+++ backend/src/config.py
@@ -6,10 +6,7 @@ from src.config_schema import Settings
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 settings_path = os.getenv("SETTINGS_PATH", BASE_DIR / ".env")
-print(Path(settings_path))
 
 settings: Settings = (
     Settings.from_env(settings_path) if Path(settings_path).exists() else Settings()  # type: ignore
 )
-print(settings.db_url.get_secret_value())
-print(settings.jwt_secret_key.get_secret_value())
